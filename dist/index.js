0e75eb18d4cfe02947f37117e690fc200dceb169
shubhangini tripathy <shubhangini.tripathy@datagrokr.com>
2019-06-06 13:01:52 +0530

    did some type tests with flow

---
+++ dist/index.js
@@ -0,0 +1,17 @@
+let name = 'shubhi';
+
+//INTERFACE
+
+let person;
+
+person = {
+    name: 'shubhi',
+    age: 25
+
+};
+
+//FUNCTIONS
+function checkType(name) {
+    return 'hello' + name;
+}
+console.log(checkType('shubhi'));
